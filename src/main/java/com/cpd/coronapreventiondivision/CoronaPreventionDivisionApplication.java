fd7b3916b3a0557235a4bb7ae2c9b38ffa54e182
chia <58979857+chia030@users.noreply.github.com>
2021-05-10 20:51:07 +0200

    Init commit

---
+++ src/main/java/com/cpd/coronapreventiondivision/CoronaPreventionDivisionApplication.java
@@ -0,0 +1,13 @@
+package com.cpd.coronapreventiondivision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronaPreventionDivisionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoronaPreventionDivisionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cpd/coronapreventiondivision/CoronaPreventionDivisionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cpd.coronapreventiondivision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronaPreventionDivisionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
