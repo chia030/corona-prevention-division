54a0b5eeaa508547f14f3a493ea2ba4d00d6f14e
justaszdanavicius <zdanaviciusjustas1@gmail.com>
2021-05-10 23:37:31 +0200

    Model implementation

---
+++ src/main/java/Model/CenterType.java
@@ -0,0 +1,7 @@
+package Model;
+
+public enum CenterType {
+    PCR_TEST,
+    MODERNA_VACCINE,
+    COMIRNATY_VACCINE
+}
---
+++ src/main/java/Model/WorkDay.java
@@ -0,0 +1,71 @@
+package Model;
+
+import java.time.LocalTime;
+
+public class WorkDay {
+
+    private int workDayID;
+    private LocalTime openingTime;
+    private LocalTime closingTime;
+    private int interval; //In minutes
+    private int capacity;
+
+    public WorkDay(int workDayID, LocalTime openingTime, LocalTime closingTime, int interval, int capacity) {
+        this.workDayID = workDayID;
+        this.openingTime = openingTime;
+        this.closingTime = closingTime;
+        this.interval = interval;
+        this.capacity = capacity;
+    }
+
+    @Override
+    public String toString() {
+        return "WorkDay{" +
+                "workDayID=" + workDayID +
+                ", openingTime=" + openingTime +
+                ", closingTime=" + closingTime +
+                ", interval=" + interval +
+                ", capacity=" + capacity +
+                '}';
+    }
+
+    public int getWorkDayID() {
+        return workDayID;
+    }
+
+    public void setWorkDayID(int workDayID) {
+        this.workDayID = workDayID;
+    }
+
+    public LocalTime getOpeningTime() {
+        return openingTime;
+    }
+
+    public void setOpeningTime(LocalTime openingTime) {
+        this.openingTime = openingTime;
+    }
+
+    public LocalTime getClosingTime() {
+        return closingTime;
+    }
+
+    public void setClosingTime(LocalTime closingTime) {
+        this.closingTime = closingTime;
+    }
+
+    public int getInterval() {
+        return interval;
+    }
+
+    public void setInterval(int interval) {
+        this.interval = interval;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+}
