9c311486e9e869ad2cab61ee10440dbca833b4dc
chia <58979857+chia030@users.noreply.github.com>
2021-05-19 17:55:02 +0200

    Login function is almost completed.

--- src/main/java/com/cpd/coronapreventiondivision/Controller/LoginController.java
+++ src/main/java/com/cpd/coronapreventiondivision/Controller/LoginController.java
@@ -1,4 +1,41 @@
 package com.cpd.coronapreventiondivision.Controller;
 
+import com.cpd.coronapreventiondivision.Model.User;
+import com.cpd.coronapreventiondivision.Service.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
 public class LoginController {
+
+    @Autowired
+    LoginService loginService;
+
+    @GetMapping("/login")
+    public String loginForm(Model model){
+        model.addAttribute("user", new User());
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String login(@ModelAttribute("user") User user) {
+        User user2 = new User(user.getUsername(), user.getPassword());
+        System.out.println(user2);
+        User user3 = loginService.fetchByUsernameAndPassword(user2.getUsername(),user2.getPassword());
+        System.out.println(user3);
+        System.out.println(user3 == null ? "wrong credentials" : "successful");
+
+        if (user3!=null && user3.getLevel().equals(User.UserType.SECRETARY)) {
+            return "redirect:/home-secretary";
+        }
+
+        else if(user3!=null && user3.getLevel() == User.UserType.ADMIN) {
+            return "redirect:/home-admin";
+        }
+
+        else return "login";
+    }
+
 }
--- src/main/java/com/cpd/coronapreventiondivision/Controller/ReportController.java
+++ src/main/java/com/cpd/coronapreventiondivision/Controller/ReportController.java
@@ -1,4 +1,18 @@
 package com.cpd.coronapreventiondivision.Controller;
 
+import com.cpd.coronapreventiondivision.Model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class ReportController {
+
+    @GetMapping("/home-secretary")
+    public String homeSecretary() {
+        return "logging/secretary-landing";
+    }
+
 }
