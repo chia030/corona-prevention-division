d8a3f945ab093ee4b39c5b4f8240f2082751c71f
chia <58979857+chia030@users.noreply.github.com>
2021-05-20 22:27:57 +0200

    Login function is almost completed.

--- src/main/java/com/cpd/coronapreventiondivision/Controller/AdminController.java
+++ src/main/java/com/cpd/coronapreventiondivision/Controller/AdminController.java
@@ -1,15 +1,18 @@
 package com.cpd.coronapreventiondivision.Controller;
 
+import com.cpd.coronapreventiondivision.Model.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class AdminController {
 
-    @GetMapping("/home-admin")
-    public String homeAdmin() {
-        return "admin/admin-landing";
-    }
+//    @GetMapping("/home-admin")
+//    public String homeAdmin(@RequestParam(name = "user") User user, Model model) {
+//        model.addAttribute("user", user);
+//        return "admin/admin-landing";
+//    }
 
 }
